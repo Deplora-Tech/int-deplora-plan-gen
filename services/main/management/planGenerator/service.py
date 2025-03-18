ff65228f6d1ef0a7bf4b89d199517f421b1596c1
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2025-03-18 12:58:30 +0530

    Fix condition check for missing information in plan generation logic

--- services/main/management/planGenerator/service.py
+++ services/main/management/planGenerator/service.py
@@ -59,7 +59,7 @@ class PlanGeneratorService:
                 parsed_files, parsed_file_content = self.file_parser.parse(
                     deployment_solution
                 )
-            if missing_information:
+            if missing_information and missing_information != []:
                 print("missings:", missing_information)
                 missing_information = self.file_parser.parse_json(missing_information)
             return (
